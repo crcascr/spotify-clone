ff2f3df1c0248e526c51c241f352e95a76e4c093
crcascr <cristianj_20@hotmail.com>
2024-04-12 18:02:53 -0500

    Change music source in Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -14,21 +14,25 @@ export const Play = () => (
 );
 
 function Player() {
-  const { isPlaying, setIsPlaying } = usePlayerStore((state) => state);
-  const [currentSong, setCurrentSong] = useState(null);
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
+    (state) => state
+  );
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = `/music/1/01.mp3`;
-  }, []);
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const {song,playlist,songs} = currentMusic
+    if(song){
+      const src=`/music/${playlist?.id}/0${song.id}.mp3`
+      audioRef.current.src=src
+      audioRef.current.play()
+    }
+  },[currentMusic])
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-      audioRef.current.volume = 0.5;
-    }
     setIsPlaying(!isPlaying);
   };
 
