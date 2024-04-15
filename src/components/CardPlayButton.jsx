6ad05f09bfb945ef2d6a8cea7d3a4c6ae54f513c
crcascr <cristianj_20@hotmail.com>
2024-04-15 12:22:54 -0500

    Added songs table to playlist page

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,14 +2,13 @@ import { Pause, Play } from "./Player";
 
 import { usePlayerStore } from "@/store/playerStore";
 
-function CardPlayButton({ id }) {
+function CardPlayButton({ id, size = "small" }) {
   const { currentMusic, isPlaying, setCurrentMusic, setIsPlaying } =
     usePlayerStore((state) => state);
 
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
   const handleClick = () => {
-
     if (isPlayingPlaylist) {
       setIsPlaying(false);
       return;
@@ -18,19 +17,24 @@ function CardPlayButton({ id }) {
     fetch(`/api/get-info-playlist-json?id=${id}`)
       .then((res) => res.json())
       .then((data) => {
-          const {songs, playlist} = data
-          setIsPlaying(true);
-          setCurrentMusic({playlist, songs, song: songs[0]})
-      })
-
+        const { songs, playlist } = data;
+        setIsPlaying(true);
+        setCurrentMusic({ playlist, songs, song: songs[0] });
+      });
   };
+
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-6 h-6";
 
   return (
     <button
-      className="card-play-button rounded-full bg-green-500 p-3"
+      className="card-play-button rounded-full bg-green-500 p-3 hover:scale-105 transition hover:bg-green-400"
       onClick={handleClick}
     >
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   );
 }
